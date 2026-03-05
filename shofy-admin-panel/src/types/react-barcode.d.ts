d770abf0a30dfc8cedd6060b6b550a3499d59459
DevXPanda <satyamkumarpandey4567@gmail.com>
2026-03-05 14:39:35 +0530

    fix deployemnt

---
+++ shofy-admin-panel/src/types/react-barcode.d.ts
@@ -0,0 +1,28 @@
+declare module "react-barcode" {
+    import React from "react";
+
+    interface BarcodeProps {
+        value: string;
+        width?: number;
+        height?: number;
+        format?: string;
+        displayValue?: boolean;
+        fontSize?: number;
+        font?: string;
+        textAlign?: string;
+        textPosition?: string;
+        textMargin?: number;
+        background?: string;
+        lineColor?: string;
+        margin?: number;
+        marginTop?: number;
+        marginBottom?: number;
+        marginLeft?: number;
+        marginRight?: number;
+        flat?: boolean;
+        renderer?: string;
+    }
+
+    const Barcode: React.FC<BarcodeProps>;
+    export default Barcode;
+}
